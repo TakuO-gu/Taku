352c17d862fd28ca436e0bc5af10fc319c1ff8de
Takuma <t.o.tmk.ml@gmail.com>
2023-12-22 09:23:23 +0900

    worksに案内矢印を追加
    philosophyBGを一枚に統合

--- assets/js/works.js
+++ assets/js/works.js
@@ -1,9 +1,9 @@
-window.onload = function() {
-  setTimeout(function() {
-      let elements = document.getElementsByTagName("*");
-      for (let i = 0; i < elements.length; i++) {
-          elements[i].classList.add("ready");
-      }
+window.onload = function () {
+  setTimeout(function () {
+    let elements = document.getElementsByTagName("*");
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.add("ready");
+    }
   }, 1);
 };
 
@@ -11,32 +11,32 @@ let startPosition = 0;
 let isDragging = false;
 const backIndicatorElement = document.querySelector(".back-indicator .circle");
 
-backIndicatorElement.addEventListener("mousedown", function(event) {
+backIndicatorElement.addEventListener("mousedown", function (event) {
   startPosition = event.clientX;
   isDragging = true;
 });
 
-document.addEventListener("mousemove", function(event) {
+document.addEventListener("mousemove", function (event) {
   if (isDragging) {
-      event.preventDefault();
-      let displacement = event.clientX - startPosition;
-      backIndicatorElement.style.transform = `translateX(${displacement}px)`;
+    event.preventDefault();
+    let displacement = event.clientX - startPosition;
+    backIndicatorElement.style.transform = `translateX(${displacement}px)`;
 
-      let threshold = 0.2 * window.innerWidth;
-      if (displacement > threshold) {
-          window.history.back();
-      }
+    let threshold = 0.2 * window.innerWidth;
+    if (displacement > threshold) {
+      window.history.back();
+    }
   }
 });
 
-document.addEventListener("mouseup", function() {
+document.addEventListener("mouseup", function () {
   if (isDragging) {
-      isDragging = false;
-      backIndicatorElement.style.transform = "translateX(0px)";
+    isDragging = false;
+    backIndicatorElement.style.transform = "translateX(0px)";
   }
 });
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   let mainImage = document.querySelector("#main-image .main-image");
   let windowHeight = window.innerHeight;
   let scrollRatio = window.pageYOffset / windowHeight;
@@ -46,29 +46,33 @@ window.addEventListener("scroll", function() {
   mainImage.style.transform = `translateZ(0) scale(${scale})`;
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  let gallery = document.querySelector("#showcase .gallery ul");
-  let description = document.querySelector("#showcase .description");
-  let isMobile = () => window.innerWidth <= 820;
-  let isDescriptionInView = () => description.getBoundingClientRect().bottom <= (window.innerHeight || document.documentElement.clientHeight);
+// 最後の子要素を取得
+let ulElement = document.querySelector("#showcase .gallery ul");
+let galleryElement = document.querySelector("#showcase .gallery");
+let lastChild = ulElement.lastElementChild;
 
-  if (isMobile()) {
-      gallery.style.overflowY = "hidden";
-  }
-
-  window.addEventListener("scroll", function() {
-      if (isMobile() && isDescriptionInView()) {
-          gallery.style.overflowY = "scroll";
-      }
+// Intersection Observerのコールバック
+let observerCallback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // 要素がビューポート内にある場合、.lastChildAppearedを追加
+      galleryElement.classList.add("lastChildAppeared");
+    } else {
+      // 要素がビューポートから外れた場合、.lastChildAppearedを削除
+      galleryElement.classList.remove("lastChildAppeared");
+    }
   });
+};
 
-  window.addEventListener("resize", function() {
-      if (isMobile()) {
-          if (!isDescriptionInView()) {
-              gallery.style.overflowY = "hidden";
-          }
-      } else {
-          gallery.style.overflowY = "";
-      }
-  });
-});
+// Intersection Observerのオプション
+let options = {
+  root: null, // ビューポートをルートとする
+  rootMargin: "0px",
+  threshold: 0.1, // 少しでも表示されたらトリガー
+};
+
+// Intersection Observerの作成
+let observer = new IntersectionObserver(observerCallback, options);
+
+// 最後の子要素を監視
+observer.observe(lastChild);
